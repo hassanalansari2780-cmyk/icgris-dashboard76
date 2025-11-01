6288fab42078078258a682fcd93d32d74f8aee75
hassanalansari2780-cmyk <hassanalansari2780@gmail.com>
2025-11-01 22:59:12 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -800,7 +800,8 @@ const claimsFiltered = React.useMemo(() => {
       </button>
     </div>
   }
-/>
+></CardHeader>
+
 
         <button className="rounded-full bg-gray-100 px-4 py-2 text-sm font-semibold hover:bg-gray-200">
           Export CSV
